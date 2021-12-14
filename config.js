54d3edf6c6f78d699f59e1f1ee1a541931117fd1
doantranvietha <96114378+doantranvietha@users.noreply.github.com>
2021-12-14 17:11:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Sin trào pạn tui :))",
     introTitle: 'Lưu Thị Kiều Anh',
-    introDesc: `Chặng đường tương lai phía trước, chắc chắn ko dễ dàng
-    Chỉ cần bên nhau ta bước, có khó khăn cũng chẳng màng
-    Rồi ngày bình yên sẽ tới, mình ngồi bên hiên vắng
-    Dưới ánh hoàng hôn cùng tách trà thơm
-    Mọi ưu phiền chẳng còn vương
+    introDesc: `Chặng đường tương lai phía trước, chắc chắn ko dễ dàng.
+    Chỉ cần bên nhau ta bước, có khó khăn cũng chẳng màng.
+    Rồi ngày bình yên sẽ tới, mình ngồi bên hiên vắng.
+    Dưới ánh hoàng hôn cùng tách trà thơm.
+    Mọi ưu phiền chẳng còn vương.
     Chỉ có hai ta mộng mơ`,
-    btnIntro: ':))',
+    btnIntro: 'Ehehe :))',
     title: 'Love',
     desc: 'Yêu nhau nhé pạn thân',
     btnYes: 'Ok',
     btnNo: 'Không',
     question: 'Mình bên nhau bao lâu rồi nhỉ???',
-    btnReply: 'Gần 9 năm rồi',
-    reply: 'Yêu nhé???',
+    btnReply: 'Yêu nhé???',
+    reply: 'Gần 9 năm rồi',
     mess: 'Ok chốt đơn',
     messDesc: 'Cuối tuần thi rồi kìa lo học đi condi yêu đương qq',
     btnAccept: 'clm :))',
